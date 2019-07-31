d08394d3a0b43a27d79b98f56aed8bb1967bc270
n@zgul <n@zgul.me>
2019-07-31 15:12:47 +0100

    Project setup

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+ #include <signal.h>
+ #include <unistd.h>
+ #include <iostream>
+ #include <fstream>
+ #include <string>
+ #include <sys/types.h>
+ #include <sys/ipc.h>
+ #include <sys/sem.h>
+
+ using namespace std;
+ #if !defined __APPLE__
+ using namespace __gnu_cxx;
+ #endif
+
+ int main(int argc, char* argv[]) {
+         return 0;
+ }
+
